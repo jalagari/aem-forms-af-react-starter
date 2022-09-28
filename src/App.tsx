77346654eafc41e418485e8c829efee92363c65b
Vijay Kumar J <vijayjalagari@Vijays-MacBook-Pro.local>
2022-09-28 12:29:15 +0530

    Rendering form json using Adaptive Form super component

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AdaptiveForm } from "@aemforms/af-react-renderer";
+import { mappings } from "@aemforms/af-react-components";
+import json from './forms/loan-form-wizard.form.json';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='header-logo'><h2>{json?.metadata?.name || "Adaptive Form"}</h2></div>
+      <div className="app">
+        <AdaptiveForm mappings={mappings} formJson={json} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider as Spectrum3Provider, defaultTheme } from '@adobe/react-spectrum'
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Spectrum3Provider theme={defaultTheme}>
+    <App />
+  </Spectrum3Provider>
+);
